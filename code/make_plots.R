f522ce9be2c102bd8395b562e7a028ad195e5985
Joël Koelewijn <joel.koelewijn19999@gmail.com>
2023-08-09 22:35:20 +0200

    Creating get_AIC_scenario
    Created get_AIC_scenario. Also started a rough outline of how get_AIC_time and get_AIC_no_of_sampl_loc will work together.

--- code/make_plots.R
+++ code/make_plots.R
@@ -18,20 +18,59 @@ load("data/R_outputs/ageDepthModelsScenariosAandB.Rdata")
 load("data/R_outputs/results_modes_of_evolution.Rdata")
 
 #### Helper functions ####
-get_AIC_scenario = function(scenario, simulated_mode){
+get_AIC_scenario = function(basin, simulated_mode){
+  
   #' 
   #' @title Get AIC vals from model output for plotting
   #' 
-  #' @param scenario: "A" or "B": the scenario of interest
+  #' @param basin: "A" or "B": the scenario of interest. Named 'basin' instead of 'scenario due to the second already being used.
   #' @param simulated_mode: "stasis", "Brownian motion", "weak Brownian drift", 
   #' or "strong Brownian drift". True (= simulated) mode of evolution for which AIC is supposed to be extracted
   #' 
   #' @return An object that can be passed to ggplot to generate boxplots of AIC in strat. domain
+
+#Creating an array ready for all the input data:
+  AkaikeWtArrayStrat <- array( 
+    data = NA,
+    dim = c(length(scenarioNames), length(examinedBasinPositions), length(simulatedEvoModes), length(testedEvoModes), noOfTests),
+    dimnames = list(
+      scenarioNames,
+      examinedBasinPositions,
+      simulatedEvoModes,
+      testedEvoModes,
+      NULL
+    )
+  )
+
+#This part fills the just created array with the Akaike data:
+  for (scenario in scenarioNames) { 
+    for (dist in examinedBasinPositions) {
+      for (trueMode in simulatedEvoModes) {
+        for (resMode in testedEvoModes) {
+          for (i in 1:noOfTests) {
+            AkaikeWtArrayStrat[scenario, dist, trueMode, resMode, i] <- testResultsStrat[[scenario]][[dist]][[trueMode]][[i]]$testRes$modelFits[resMode, "Akaike.wt"]
+          }
+        }
+      }
+    }
+  }
+  
+#This part takes only the required Akaike data from the array and puts it in a format accepted by ggplot:  
+  df=data.frame(position=NULL,testedMode=NULL,AIC=NULL) 
+  for (pos in examinedBasinPositions){
+    for (testedEvoMode in testedEvoModes){
+      df=rbind(df,data.frame(position=as.factor(rep(pos,length(AkaikeWtArrayStrat[1,1,1,1,]))),
+                             testedEvoMode=rep(testedEvoMode,length(AkaikeWtArrayStrat[1,1,1,1,])),
+                             AIC=AkaikeWtArrayStrat[basin,pos,simulated_mode,testedEvoMode,]))
+    }
+  }
+  return(df) #The return, ready for input in ggplot.
 }
 
+ 
 get_AIC_no_of_sampl_loc= function(no_of_sampl_loc, simulated_mode, scenario){
   #' 
-  #' @title get AIC from test results for specified number of sampling locatinos
+  #' @title get AIC from test results for specified number of sampling locations
   #' 
   #' @param no_of_sampl_loc : string, element of noOfSamplingLoc
   #' @param simulated_mode: "stasis", "Brownian motion", "weak Brownian drift", 
@@ -42,18 +81,22 @@ get_AIC_no_of_sampl_loc= function(no_of_sampl_loc, simulated_mode, scenario){
   stopifnot(no_of_sampl_loc %in% noOfSamplingLoc) # check if data from sampl. location is available
 }
 
-get_AIC_time = function(no_of_sampl_loc, simulated_mode, scenario){
+get_AIC_time = function(basin, simulated_mode){
   #' 
   #' @title Get AIC vals from model output for plotting
   #' 
-  #' @param no_of_sampl_loc: char vector, subset of noOfSamplingLoc: number of sampling points
+  #' @param basin: "A" or "B": the scenario of interest. Named 'basin' instead of 'scenario due to the second already being used.
   #' @param simulated_mode: "stasis", "Brownian motion", "weak Brownian drift", 
   #' or "strong Brownian drift". True (= simulated) mode of evolution for which AIC is supposed to be extracted
   #' 
-  #' @description combine outputs from the the function get_AIC_no_of_sampl_loc
+  #' @description combine outputs from the function get_AIC_no_of_sampl_loc
   #' 
   #' @return An object that can be passed to ggplot to generate boxplots of AIC in time domain
   #' 
+  #' Notes to self(Joël):
+  #' noOfSamplingLoc= All investigated lengths of time series
+  #' ts_length_mat = The lengths of the time series in all locations in all basins, table form.
+  #' no_of_sampl_loc is created in get_AIC_time for all locations and then used to get the correct AIC values.
   #
   
   
