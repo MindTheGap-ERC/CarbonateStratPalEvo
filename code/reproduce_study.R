5719a8d29be1c2d35eaec2dde26fe553045fd5ed
Niklas Hohmann <67792281+NiklasHohmann@users.noreply.github.com>
2023-08-22 07:25:08 +0200

    description of generated data

--- code/reproduce_study.R
+++ code/reproduce_study.R
@@ -1,29 +1,53 @@
-## set seed for repeatability
-set.seed(1)
-
-## load utility functions
+#### load utility functions ####
 source("code/utils.R")
 
-## download data from osf
+#### download matlab data from osf ####
+# requires files:
+# none
+# downloads files:
+# - data/matlab_outputs/scenarioA_matlab_outputs.mat
+# - data/matlab_outputs/scenarioB_matlab_outputs.mat
+# - data/matlab_outputs/scenarioA_and_B_matlab_to_R.mat
+# file size: < 200 Mb
 download_data = FALSE
 if(download_data){
   source("code/download_data_from_osf.R")
 }
 
-## import ADMs from matlab
+#### import ADMs from matlab ####
+# requires files:
+# - data/matlab_outputs/scenarioA_matlab_outputs.mat
+# - data/matlab_outputs/scenarioB_matlab_outputs.mat
+# - data/matlab_outputs/scenarioA_and_B_matlab_to_R.mat
+# generates files:
+# - ageDepthModelsScenariosAandB.RData
+# file size: < 10 Mb
 process_adms = FALSE
 if(process_adms){
   source("code/process_adm_from_matlab.R")
 }
 
-## run simulations and tests
+#### run simulations and tests ####
+# requires files:
+# - ageDepthModelsScenariosAandB.RData
+# generates files:
+# - results_modes_of_evolution.RData
+# file size: < 40 Mb
+# computation time: hours to days!
 run_sim_and_test = FALSE
 if(run_sim_and_test){
   #! Warning, will take hours to compute
   source("code/simulate_and_test_modes_of_evolution.R")
 }
 
-## make plots
+#### make plots ####
+# requires files:
+# - results_modes_of_evolution.RData
+# generates files:
+# - multiple .pdf files in figs/R/
+# file size: combined a few Mb
+# computation time:
+# - few minutes
 make_plots = TRUE
 if(make_plots){
   source("code/make_plots.R")
